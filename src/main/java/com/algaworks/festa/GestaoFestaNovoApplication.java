1476c92fd621f75bd9d70c3247663adbb05564da
Nirvana16 <zedanilo161@gmail.com>
2017-10-26 10:01:44 -0200

    Criado Controller, Projeto inicial e pagina HTML Crua

---
+++ src/main/java/com/algaworks/festa/GestaoFestaNovoApplication.java
@@ -0,0 +1,12 @@
+package com.algaworks.festa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFestaNovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFestaNovoApplication.class, args);
+	}
+}
